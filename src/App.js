b3a2d655ddc5b52a060f7cffbe3331f4e1ec8679
munadi1406 <munadifathullah123@gmail.com>
2022-11-11 19:14:40 +0800

    first push portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react"
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Profile from "./components/Profile"
+import Project from "./components/Project"
+import Contact from "./components/Contact"
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <Navbar/>
+      <Suspense fallback={<h1>Loading.....</h1>}>
+        <Routes>
+            <Route path="/" exact="true" element={<Home/>}/>
+            <Route path="profile" element={<Profile/>}/>
+            <Route path="project" element={<Project/>}/>
+            <Route path="contact" element={<Contact/>}/>
+        </Routes>
+      </Suspense>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,130 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "https://kit.fontawesome.com/6b4714a33f.js";
+import "./css/style.css"
+
+const Navbar = () => {
+  let darkMode = 0;
+
+  function mode() {
+    const navbar = document.querySelector(".navbar");
+    const navbar2 = document.querySelector(".navbar-mobile-version");
+    const nTitle = document.querySelector(".navbar-title");
+    const cTitle = document.querySelector(".color-title");
+    const icon = document.querySelectorAll(".material-symbols-outlined");
+    const a = document.querySelectorAll(".title-icon");
+    const spanDefault = document.querySelectorAll(".sDefault");
+    const iconDarkMode = document.querySelector(".icon-dark-mode");
+    const navbarDefault = document.querySelector('.nav-default');
+    const wrapperNavbarDefault = document.querySelector(".wrapper-navbar-default");
+
+    navbar2.classList.toggle("dark-mode");
+    navbar.classList.toggle("dark-mode");
+    nTitle.classList.toggle("color-dark");
+    cTitle.classList.toggle("color-dark");
+    wrapperNavbarDefault.classList.toggle('dark-mode');
+    navbarDefault.classList.toggle('dark-mode-mobile');
+
+    darkMode += 1;
+    if (darkMode === 10) {
+      darkMode -= darkMode;
+    }
+    if (darkMode % 2 === 0) {
+      cTitle.textContent = "Dark Mode";
+      iconDarkMode.innerHTML = "dark_mode";
+    } else {
+      iconDarkMode.innerHTML = "wb_sunny";
+      cTitle.textContent = "Light Mode";
+    }
+
+    spanDefault.forEach((e) => {
+      e.classList.toggle("color-dark");
+    });
+    icon.forEach((e) => {
+      e.classList.toggle("color-dark");
+    });
+    a.forEach((e) => {
+      e.classList.toggle("color-dark");
+    });
+
+    
+  }
+  return (
+    <nav className="navbar">
+    <div className="wrapper-navbar-default">
+      <nav className="nav-default">
+        <div className="navbar-brand">
+          <i className="fa-solid fa-droplet"></i>
+          <div className="navbar-title">Water</div>
+        </div>
+        <ul className="ul-default-version">
+          <li className="link-default">
+            <NavLink to={"/"}>
+              <span className="sDefault" data-hover="Home">
+                Home
+              </span>
+            </NavLink>
+          </li>
+          <li className="link-default">
+            <NavLink to={"project"}>
+              <span className="sDefault" data-hover="Project">
+                Project
+              </span>
+            </NavLink>
+          </li>
+          <li className="link-default">
+            <NavLink to={"#"}>
+              <span className="sDefault" data-hover="Resume">
+                Resume
+              </span>
+            </NavLink>
+          </li>
+          <li className="link-default">
+            <NavLink to={"contact"}>
+              <span className="sDefault" data-hover="Contact">
+                Contact
+              </span>
+            </NavLink>
+          </li>
+        </ul>
+        <div className="color-mode" onClick={mode}>
+          <span className="material-symbols-outlined icon-dark-mode">
+            dark_mode
+          </span>
+          <div className="color-title">Dark Mode</div>
+        </div>
+      </nav>
+    </div>
+      {/* <!-- navbar untuk versi mobile --> */}
+      <nav className="navbar-mobile-version">
+        <ul>
+          <li className="link">
+            <NavLink to={"/"} className="a">
+              <span className="material-symbols-outlined">home</span>
+              <span className="title-icon">Home</span>
+            </NavLink>
+          </li>
+          <li className="link">
+            <NavLink to={"/project"} className="a">
+              <span className="material-symbols-outlined">layers</span>
+              <span className="title-icon">Project</span>
+            </NavLink>
+          </li>
+          <li className="link">
+            <NavLink to={"/resume"} className="a">
+              <span className="material-symbols-outlined">library_books</span>
+              <span className="title-icon">Resume</span>
+            </NavLink>
+          </li>
+          <li className="link">
+            <NavLink to={"/contact"} className="a">
+              <span className="material-symbols-outlined">contact_mail</span>
+              <span className="title-icon">Contact</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </nav>
+  );
+};
+export default Navbar;
