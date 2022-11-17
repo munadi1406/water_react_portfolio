e2b54acd6ed69f724c8995ac808731eda4f30a7d
munadi1406 <munadifathullah123@gmail.com>
2022-11-17 10:15:31 +0800

    menambakan limit http

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 const Profile = () => {
   const [data,setData] = useState([]);
   useEffect(()=>{
-      axios.get('https://dummyjson.com/products')
+      axios.get('https://dummyjson.com/products?limit=100')
       .then(res => {
         setData(res.data.products)
       })
